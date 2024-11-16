8fd81177568d941828852547de07d5057a2f4f01
Santiago Fontana <santiagofontana61@gmail.com>
2024-11-16 15:22:09 -0300

    Update Ejercicio-3.2.py
    Agregando el metodo capitalize

--- Ejercicios-3-Python/Ejercicio-3.2.py
+++ Ejercicios-3-Python/Ejercicio-3.2.py
@@ -2,7 +2,7 @@
 colores_primarios = ["Rojo", "Amarillo","Azul"]
 
 def esPrimario(color):
-    if color in colores_primarios:
+    if color.capitalize() in colores_primarios:
         print(f"El color {color} es primario.")
     else:
         print(f"El color {color} no es primario.")
